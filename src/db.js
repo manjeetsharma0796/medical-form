7970c8cd07f20d550bec214b7c957e25c3ca3645
manjeetsharma0796 <manjeetsharma0796@gmail.com>
2024-04-14 22:54:42 +0530

    db connected and functional

---
+++ src/db.js
@@ -0,0 +1,48 @@
+const { MongoClient} = require('mongodb');
+
+const url = 'mongodb+srv://manjeet0796:manjeet0796@cluster0.irnuxnb.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(url);
+const dbName = "MedicalDb";               //this will be bookFairDb
+const collectionName = "PatientHistory";   //this will be users   in future
+
+const displayAll = () => {
+    return client.connect()             // to see all data in collection
+    .then(() => {
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        console.log("============FOUND AND CONNECTED=============");
+
+        return collection.find().toArray();
+    })
+    .then((documents) => {
+        console.log(documents);
+    })
+    .catch((err) =>  {
+        console.log(err, "error happened");
+    }).finally(() => {
+        return client.close();
+    })
+}
+
+const user = {name: "raj", age: 33, gender:"M", medicalHistory:"No previous issues"};         //this is details sample
+
+const insertUser = (user) => {
+    return client.connect()
+    .then(() => {
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);        // Hardcoded user data
+    
+        return collection.insertOne(user);
+    })
+    .then((result)=> {
+        console.log('User inserted successfully:', result);
+    })
+};
+
+module.exports = {
+    insertUser
+}
+
+// insertUser(user);
+displayAll();
