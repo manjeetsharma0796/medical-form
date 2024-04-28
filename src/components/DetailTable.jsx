dbce09ba8e0361bbc74755286af625ace7335104
rajsha10 <rajsha8617@gmail.com>
2024-04-28 22:45:06 +0530

    adding the patient details table

--- src/components/DetailTable.jsx
+++ src/components/DetailTable.jsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react';
-const API = "https://jsonplaceholder.typicode.com/users";
+import UserData from '../miniCompo/UserData';
+import './componentsCss/DetailTable.css';
+const API = "#";
+
 
 const DetailTable = () => {
+
+    const [users, setUsers] = useState([]);
 
     const fetchUsers = async (url) => {
         try {
             const res = await fetch(url);
             const data = await res.json();
+            // console.log(data);
+
+            if(data.length > 0){
+                setUsers(data);
+            }
         }catch (e) {
             console.error(e);
         }
@@ -18,7 +28,22 @@ const DetailTable = () => {
 
     return (
         <>
-            Hello Baby
+            <table className="detailTable">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Phone No.</th>
+                        <th>Diabetic</th>
+                        <th>Smoker</th>
+                        <th>Hypertension</th>
+                        <th>CBG</th>
+                        <th>ECG</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <UserData users={users} />
+                </tbody>
+            </table>
         </>
     )
 }
---
+++ src/miniCompo/UserData.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const UserData = ({users}) => {
+  return (
+    <>
+        {
+            users.map((curUser) => {
+                const {name, phone, diab, smok, hyper, cbg, ecg} = curUser;
+
+                return (
+                    <tr>
+                        <td>{name}</td>
+                        <td>{phone}</td>
+                        <td>{diab ? "yes" : "no"}</td>
+                        <td>{smok ? "yes" : "no"}</td>
+                        <td>{hyper ? "yes" : "no"}</td>
+                        <td>{cbg ? "yes" : "no"}</td>
+                        <td>{ecg ? "yes" : "no"}</td>
+                    </tr>
+                )
+            })
+        }
+    </>
+  )
+}
+
+export default UserData;
